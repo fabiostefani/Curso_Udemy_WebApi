f9f802c818fac69207b1b29afedc4470c510ee95
fabiostefani@gmail.com <fabiostefani@gmail.com>
2019-02-09 14:35:19 -0200

    Ajustes

---
+++ fabiostefani.io.WebApp.Api/Startup.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Owin;
+using Swashbuckle.Application;
+
+[assembly: OwinStartup(typeof(fabiostefani.io.WebApp.Api.Startup))]
+
+namespace fabiostefani.io.WebApp.Api
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            // Serviços e configuração da API da Web
+            //config.EnableCors();
+
+            // Rotas da API da Web
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            ConfigureWebApi(config);
+
+            app.UseCors(CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+
+        public static void ConfigureWebApi(HttpConfiguration config)
+        {
+            // Remove o XML
+            var formatters = config.Formatters;
+            formatters.Remove(formatters.XmlFormatter);
+
+            //Compacta retorno de cada requisição realizada para api
+            //config.MessageHandlers.Insert(0, new ServerCompressionHandler(new GZipCompressor(), new DeflateCompressor()));
+
+            // Modifica a identação
+            var jsonSettings = formatters.JsonFormatter.SerializerSettings;
+            jsonSettings.Formatting = Formatting.Indented;
+            jsonSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            // Modifica a serialização
+            formatters.JsonFormatter.SerializerSettings.PreserveReferencesHandling = PreserveReferencesHandling.None;
+
+
+            //SwaggerConfig.Register(config);
+
+            //Register(config);   
+            config.EnableSwagger(c => {
+                c.SingleApiVersion("v1", "fabiostefani.io.WebApp.Api");                
+                c.IncludeXmlComments(GetXmlCommentsPath());
+                
+            });
+        }
+        private static string GetXmlCommentsPath()
+        {
+            return System.AppDomain.CurrentDomain.BaseDirectory + @"\bin\fabiostefani.io.WebApp.Api.xml";
+        }
+    }
+}
